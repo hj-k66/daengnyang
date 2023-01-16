df6dccd426db293fdd9cb58557d696e1ecc6006d
JO YEJI <whdpwl2@gmail.com>
2023-01-16 15:49:16 +0900

    :tada: init: build project

---
+++ src/main/java/com/daengnyangffojjak/dailydaengnyang/DailyDaengnyangApplication.java
@@ -0,0 +1,13 @@
+package com.daengnyangffojjak.dailydaengnyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDaengnyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyDaengnyangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daengnyangffojjak/dailydaengnyang/DailyDaengnyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daengnyangffojjak.dailydaengnyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyDaengnyangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
