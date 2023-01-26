4001f13e088053977b66573455b64d91723a2216
hjx66 <hjx66@g.cnu.ac.kr>
2023-01-26 16:31:07 +0900

    ✨ feat: redis 설정 추가

--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/controller/rest/UserRestController.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/controller/rest/UserRestController.java
@@ -3,7 +3,10 @@ package com.daengnyangffojjak.dailydaengnyang.controller.rest;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.*;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserJoinRequest;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserJoinResponse;
+import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserLoginRequest;
+import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserLoginResponse;
 import com.daengnyangffojjak.dailydaengnyang.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +27,12 @@ public class UserRestController {
         return ResponseEntity.created(URI.create("/api/v1/users/"+userJoinResponse.getId()))     //성공 시 상태코드 : 201
                 .body(Response.success(userJoinResponse));
     }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<Response<UserLoginResponse>> login(@RequestBody @Valid UserLoginRequest userLoginRequest){
+        userService.login(userLoginRequest);
+    }
+
     @GetMapping(value = "/test")
     public Map<String, String> test(){
         return new HashMap<>(){{put("test", "ok");}};
--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/UserService.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/UserService.java
@@ -2,11 +2,16 @@ package com.daengnyangffojjak.dailydaengnyang.service;
 
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserJoinRequest;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserJoinResponse;
+import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserLoginRequest;
+import com.daengnyangffojjak.dailydaengnyang.domain.dto.user.UserLoginResponse;
 import com.daengnyangffojjak.dailydaengnyang.domain.entity.User;
 import com.daengnyangffojjak.dailydaengnyang.exception.ErrorCode;
 import com.daengnyangffojjak.dailydaengnyang.exception.UserException;
 import com.daengnyangffojjak.dailydaengnyang.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
 
     public UserJoinResponse join(UserJoinRequest userJoinRequest) {
@@ -34,4 +40,32 @@ public class UserService {
 
         return UserJoinResponse.from(saved);
     }
+
+    public UserLoginResponse login(UserLoginRequest userLoginRequest){
+        //1. 해당 user 있는지 검증
+        User user = userRepository.findByUserName(userLoginRequest.getUserName())
+                .orElseThrow(() -> new UserException(ErrorCode.USERNAME_NOT_FOUND));
+
+        //2. id,pw 기반으로 Authentication 객체 생성
+        UsernamePasswordAuthenticationToken authenticationToken = userLoginRequest.toAuthentication();
+
+        //3. 실제 인증
+        //DaoAuthenticationProvider class 내 additionalAuthenticationChecks() 메소드로 비밀번호 체크
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        //4.인증 정보 기반으로 JWT 토큰 생성
+        //5.RefreshToken Redis에 저장
+
+    }
+    /*
+     String userName = userLoginRequest.getUserName();
+        //해당 userName이 없는 경우
+        User user = validator.validateUser(userName);
+        //해당 password가 틀린 경우
+        if(!encoder.matches(userLoginRequest.getPassword(),user.getPassword())){
+            throw new SNSException(ErrorCode.INVALID_PASSWORD);
+        }
+        return JwtTokenUtil.createToken(user.getUserName(),secretKey,expireTimeMs);
+
+     */
 }
