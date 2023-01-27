a1346672e87a40ad61e0085bc74e2f0e65b6c053
JO YEJI <whdpwl2@gmail.com>
2023-01-27 13:03:35 +0900

    :recycle: refactor: Pet getAge(), PetService 수정
    - getAge() else 제거
    - 클래스 내부에만 쓰는 메서드 private으로 변경
    
    fix: #15

--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/domain/entity/Pet.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/domain/entity/Pet.java
@@ -2,14 +2,21 @@ package com.daengnyangffojjak.dailydaengnyang.domain.entity;
 
 import com.daengnyangffojjak.dailydaengnyang.domain.entity.enums.Sex;
 import com.daengnyangffojjak.dailydaengnyang.domain.entity.enums.Species;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 @Entity
 @Getter
@@ -17,32 +24,31 @@ import java.time.temporal.ChronoUnit;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Pet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
-    private Group group;
-    @Enumerated(EnumType.STRING)
-    private Species species;
-    private String breed;       //추후 enum으로 수정 예정
-    private String name;
-    @Enumerated(EnumType.STRING)
-    private Sex sex;
-    private LocalDate birthday;
-    private double weight;
 
-    public String getAge(){     //1년령 이상은 나이, 1년령 이하는 개월수로 반환
-        LocalDate today = LocalDate.now();
-        LocalDate birthday = this.birthday;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "group_id")
+	private Group group;
+	@Enumerated(EnumType.STRING)
+	private Species species;
+	private String breed;       //추후 enum으로 수정 예정
+	private String name;
+	@Enumerated(EnumType.STRING)
+	private Sex sex;
+	private LocalDate birthday;
+	private double weight;
 
-        long months = ChronoUnit.MONTHS.between(birthday, today); //태어난지 12개월 넘었는 지
-        long years = ChronoUnit.YEARS.between(birthday, today);
+	public String getAge() {     //1년령 이상은 나이, 1년령 이하는 개월수로 반환
+		LocalDate today = LocalDate.now();
+		LocalDate birthday = this.birthday;
 
-        if(months < 12){
-            return months + "개월";
-        } else {
-            return years +"살";
-        }
-    }
+		long months = ChronoUnit.MONTHS.between(birthday, today); //태어난지 12개월 넘었는 지
+
+		if (months < 12) {
+			return months + "개월";
+		}
+		return ChronoUnit.YEARS.between(birthday, today) + "살";
+	}
 }
