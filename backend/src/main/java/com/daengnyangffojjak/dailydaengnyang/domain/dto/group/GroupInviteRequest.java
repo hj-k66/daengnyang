b129d059916831867b8e956d5ca4e53d2388fb02
JO YEJI <whdpwl2@gmail.com>
2023-01-29 10:45:25 +0900

    :white_check_mark: test: 그룹 초대 테스트코드 작성
    - controller, service 테스트 코드 작성 및 통과
    - 그룹초대 service 로직에서 이미 존재하는 역할이면 예외 발생 추가
    
    Fixes: #15

--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/domain/dto/group/GroupInviteRequest.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/domain/dto/group/GroupInviteRequest.java
@@ -1,8 +1,12 @@
 package com.daengnyangffojjak.dailydaengnyang.domain.dto.group;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class GroupInviteRequest {
 
 	private String email;
--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/GroupService.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/GroupService.java
@@ -18,6 +18,7 @@ import com.daengnyangffojjak.dailydaengnyang.repository.PetRepository;
 import com.daengnyangffojjak.dailydaengnyang.repository.UserGroupRepository;
 import com.daengnyangffojjak.dailydaengnyang.repository.UserRepository;
 import java.util.List;
+import java.util.function.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -60,11 +61,38 @@ public class GroupService {
 		return GroupPetListResponse.from(pets);
 	}
 
+	@Transactional
+	public MessageResponse inviteMember(Long groupId, String username,
+			GroupInviteRequest groupInviteRequest) {
+		Group group = getGroupById(groupId);
+		List<UserGroup> userGroupList = getUserGroupListWithUsername(group, username);
+
+		User invited = userRepository.findByEmail(
+						groupInviteRequest.getEmail())        //email로 유저 조회
+				.orElseThrow(() -> new UserException(ErrorCode.EMAIL_NOT_FOUND));
+
+		Predicate<UserGroup> memberExist = userGroup -> invited.getUsername()
+				.equals(userGroup.getUser().getUsername());        //이미 유저가 그룹에 존재
+		Predicate<UserGroup> roleExist = userGroup -> groupInviteRequest.getRoleInGroup()
+				.equals(userGroup.getRoleInGroup());            //이미 role이 그룹에 존재
+		Predicate<UserGroup> combinedCondition = memberExist.or(roleExist);
+		if (userGroupList.stream().anyMatch(combinedCondition)) {
+			throw new GroupException(ErrorCode.INVALID_REQUEST, "이미 존재하는 회원 또는 역할입니다.");
+		}
+		UserGroup savedUserGroup = userGroupRepository.save(    //그룹에 추가
+				UserGroup.from(invited, group, groupInviteRequest.getRoleInGroup(),
+						false));   //그룹 멤버로 저장
+		return new MessageResponse(invited.getUsername() + "이(가) 그룹에 등록되었습니다.");
+	}
+
 	private Group getGroupById(Long groupId) {    //그룹 아이디로 그룹 조회, 없으면 예외 발생
 		return groupRepository.findById(groupId)
 				.orElseThrow(() -> new GroupException(ErrorCode.GROUP_NOT_FOUND));
 	}
 
+	/**
+	 * 추후 admin 계정도 가능하게 수정 예정
+	 **/
 	private List<UserGroup> getUserGroupListWithUsername(Group group,
 			String username) { //유저가 그룹 내 멤버이면 그룹유저리스트 반환
 		User user = userRepository.findByUserName(username)
@@ -77,25 +105,5 @@ public class GroupService {
 			throw new UserException(ErrorCode.INVALID_PERMISSION);
 		}
 		return userGroupList;
-	}
-
-	public MessageResponse inviteMember(Long groupId, String username,
-			GroupInviteRequest groupInviteRequest) {
-		Group group = getGroupById(groupId);
-		List<UserGroup> userGroupList = getUserGroupListWithUsername(group, username);
-
-		User invited = userRepository.findByEmail(
-						groupInviteRequest.getEmail())        //email로 유저 조회
-				.orElseThrow(() -> new UserException(ErrorCode.EMAIL_NOT_FOUND));
-		if (userGroupList.stream()                                //이미 존재하는 회원인 경우 예외 발생
-				.anyMatch(userGroup -> invited.getUsername()
-						.equals(userGroup.getUser().getUsername()))) {
-			throw new UserException(ErrorCode.INVALID_REQUEST, "이미 존재하는 회원입니다.");
-		}
-		//그룹에 추가
-		UserGroup savedUserGroup = userGroupRepository.save(
-				UserGroup.from(invited, group, groupInviteRequest.getRoleInGroup(),
-						false));   //그룹 멤버로 저장
-		return new MessageResponse(invited.getUsername() + "이(가) 그룹에 등록되었습니다.");
 	}
 }
