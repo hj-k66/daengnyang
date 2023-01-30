bb0c961ccd4afc8528f1e7fe515dc61afe4aa107
minkyoung kim <minry20415@naver.com>
2023-01-30 09:18:11 +0900

    :lipstick: style: 코드컨벤션 설정

--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/ScheduleService.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/ScheduleService.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import static com.daengnyangffojjak.dailydaengnyang.exception.ErrorCode.*;
 
 @Service
