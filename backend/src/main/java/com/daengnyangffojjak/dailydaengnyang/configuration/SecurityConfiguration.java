0693d0aec9c788af11d3d220e31bf6c96384c169
mooonju <mvv0030@gmail.com>
2023-02-01 10:33:20 +0900

    :heavy_plus_sign: add: /view 페이지 접근 허용

--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/configuration/SecurityConfiguration.java
@@ -31,6 +31,7 @@ public class SecurityConfiguration {
 				//springboot 3.0부터 security HTTP 요청 권한 승인 로직 변경
 				//Instead of using authorizeRequests, use authorizeHttpRequests
 				.cors().and().authorizeHttpRequests(authorize -> authorize
+						.requestMatchers("/view/**").permitAll()
 						.requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
 						.requestMatchers("/docs/index.html").permitAll()
 						.requestMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
