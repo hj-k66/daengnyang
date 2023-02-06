e9aace3e8e546b2d41f2b6b7a10fd231398247ba
JO YEJI <whdpwl2@gmail.com>
2023-02-06 16:16:13 +0900

    :sparkles: feat: 모니터링 레포트 기능 추가
    - 기간 입력 후 모니터링 항목별 평균값 반환하는 기능
    - 모니터링 달단위 조회도 기간 입력해서 조회하는 방식으로 변경

--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/MonitoringService.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/service/MonitoringService.java
@@ -3,16 +3,17 @@ package com.daengnyangffojjak.dailydaengnyang.service;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.monitoring.MntDeleteResponse;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.monitoring.MntGetResponse;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.monitoring.MntMonthlyResponse;
+import com.daengnyangffojjak.dailydaengnyang.domain.dto.monitoring.MntReportResponse;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.monitoring.MntWriteRequest;
 import com.daengnyangffojjak.dailydaengnyang.domain.dto.monitoring.MntWriteResponse;
 import com.daengnyangffojjak.dailydaengnyang.domain.entity.Monitoring;
 import com.daengnyangffojjak.dailydaengnyang.domain.entity.Pet;
-import com.daengnyangffojjak.dailydaengnyang.domain.entity.UserGroup;
 import com.daengnyangffojjak.dailydaengnyang.exception.ErrorCode;
 import com.daengnyangffojjak.dailydaengnyang.exception.MonitoringException;
 import com.daengnyangffojjak.dailydaengnyang.repository.MonitoringRepository;
 import com.daengnyangffojjak.dailydaengnyang.utils.Validator;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,17 +29,40 @@ public class MonitoringService {
 	@Transactional
 	public MntWriteResponse create(Long petId, MntWriteRequest mntWriteRequest, String username) {
 		Pet pet = validator.getPetWithUsername(petId, username);
+		if (monitoringRepository.existsByDate(mntWriteRequest.getDate())) {
+			throw new MonitoringException(ErrorCode.INVALID_REQUEST, "해당 날짜의 모니터링이 이미 존재합니다.");
+		}
 		Monitoring saved = monitoringRepository.save(mntWriteRequest.toEntity(pet));
 		return MntWriteResponse.from(saved);
 	}
 	@Transactional(readOnly = true)
-	public MntMonthlyResponse getMonthly(Long petId, Integer year, Integer month, String username) {
+	public MntMonthlyResponse getMonitoringList(Long petId, String fromDate, String toDate, String username) {
 		Pet pet = validator.getPetWithUsername(petId, username);
-		LocalDate start = LocalDate.of(year, month, 1);
-		LocalDate end = LocalDate.of(year, month, start.lengthOfMonth());
+		LocalDate start = getLocalDateFromString(fromDate);
+		LocalDate end = getLocalDateFromString(toDate);
+
+		long days = ChronoUnit.DAYS.between(start, end);
+		if (days < 7 || days > 93) {
+			throw new MonitoringException(ErrorCode.INVALID_REQUEST, "레포트 작성은 7일 이상, 3달 이하의 기간만 가능합니다.");
+		}
 
 		List<Monitoring> monitorings = monitoringRepository.findAllByDateBetween(start, end);
 		return MntMonthlyResponse.from(monitorings);
+	}
+
+	@Transactional(readOnly = true)
+	public MntReportResponse getReport(Long petId, String fromDate, String toDate, String username) {
+		Pet pet = validator.getPetWithUsername(petId, username);
+		LocalDate start = getLocalDateFromString(fromDate);
+		LocalDate end = getLocalDateFromString(toDate);
+
+		long days = ChronoUnit.DAYS.between(start, end);
+		if (days < 7 || days > 93) {
+			throw new MonitoringException(ErrorCode.INVALID_REQUEST, "레포트 작성은 7일 이상, 3달 이하의 기간만 가능합니다.");
+		}
+		List<Monitoring> monitorings = monitoringRepository.findAllByDateBetween(start, end);
+
+		return makeReport(monitorings);
 	}
 
 	@Transactional    //일단 pet은 바꿀 수 없음
@@ -67,6 +91,7 @@ public class MonitoringService {
 		return MntGetResponse.from(monitoring);
 	}
 
+
 	//모니터링의 해당 반려동물의 것이 맞으면 Monitoring 반환
 	private Monitoring validateMonitoringWithPetId(Long monitoringId, Long petId) {
 		Monitoring monitoring = validator.getMonitoringById(monitoringId);
@@ -76,4 +101,169 @@ public class MonitoringService {
 		}
 		return monitoring;
 	}
+
+	// 20220101 -> LocalDate.of(2022, 1, 1)
+	private LocalDate getLocalDateFromString (String date) {
+		int year = Integer.parseInt(date.substring(0, 4));
+		int month = Integer.parseInt(date.substring(4, 6));
+		int day = Integer.parseInt(date.substring(6, 8));
+
+		return LocalDate.of(year, month, day);
+	}
+
+	// ^^;;; 좋은 방법이 있으면 알려주세요.
+	private MntReportResponse makeReport (List<Monitoring> monitorings) {
+		double weightSum = 0;
+		int weightCount = 0;
+		int vomitTrue = 0;
+		int vomitCount = 0;
+		int amPillTrue = 0;
+		int amPillCount = 0;
+		int pmPillTrue = 0;
+		int pmPillCount = 0;
+		int customSymptomTrue = 0;
+		int customSymptomCount = 0;
+		String customSymptomName = "";
+
+		int feedToGramSum = 0;
+		int feedToGramCount = 0;
+		int walkCntSum = 0;
+		int walkCntCount = 0;
+		int playCntSum = 0;
+		int playCntCount = 0;
+		int urinationSum = 0;
+		int urinationCount = 0;
+		int defecationSum = 0;
+		int defecationCount = 0;
+		int respiratoryRateSum = 0;
+		int respiratoryRateCount = 0;
+		int customIntSum = 0;
+		int customIntCount = 0;
+		String customIntName = "";
+		for (Monitoring monitoring : monitorings) {
+			Double weight = monitoring.getWeight();
+			if (weight != null) {
+				weightSum += weight;
+				weightCount += 1;
+			}
+			Boolean vomit = monitoring.getVomit();
+			if (vomit != null) {
+				vomitCount += 1;
+				if (vomit) {
+					vomitTrue += 1;
+				}
+			}
+			Boolean amPill = monitoring.getAmPill();
+			if (amPill != null) {
+				amPillCount += 1;
+				if (amPill) {
+					amPillTrue += 1;
+				}
+			}
+			Boolean pmPill = monitoring.getPmPill();
+			if (pmPill != null) {
+				pmPillCount += 1;
+				if (pmPill) {
+					pmPillTrue += 1;
+				}
+			}
+			Boolean customSymptom = monitoring.getCustomSymptom();
+			if (customSymptom != null && monitoring.getCustomSymptomName() != null) {
+				if(!monitoring.getCustomSymptomName().equals(customSymptomName)) {
+					customSymptomName = monitoring.getCustomSymptomName();
+					customSymptomCount = 0;
+					customSymptomTrue = 0;
+				}
+				customSymptomCount += 1;
+				if (customSymptom) {
+					customSymptomTrue += 1;
+				}
+			}
+
+			Integer feedToGram = monitoring.getFeedToGram();
+			if (feedToGram != null) {
+				feedToGramCount += 1;
+				feedToGramSum += feedToGram;
+			}
+
+			Integer walkCnt = monitoring.getWalkCnt();
+			if (walkCnt != null) {
+				walkCntCount += 1;
+				walkCntSum += walkCnt;
+			}
+			Integer playCnt = monitoring.getPlayCnt();
+			if (playCnt != null) {
+				playCntCount += 1;
+				playCntSum += playCnt;
+			}
+			Integer urination = monitoring.getUrination();
+			if (urination != null) {
+				urinationCount += 1;
+				urinationSum += urination;
+			}
+			Integer defecation = monitoring.getDefecation();
+			if (defecation != null) {
+				defecationCount += 1;
+				defecationSum += defecation;
+			}
+			Integer respiratoryRate = monitoring.getRespiratoryRate();
+			if (respiratoryRate != null) {
+				respiratoryRateCount += 1;
+				respiratoryRateSum += respiratoryRate;
+			}
+			Integer customInt = monitoring.getCustomInt();
+			if (customInt != null && monitoring.getCustomIntName() != null) {
+				if(!monitoring.getCustomIntName().equals(customIntName)) {
+					customIntName = monitoring.getCustomIntName();
+					customIntCount = 0;
+					customIntSum = 0;
+				}
+				customIntCount += 1;
+				customIntSum += customInt;
+			}
+		}
+		double weightAvg = 0.0;
+		if (weightCount != 0) {weightAvg = weightSum/weightCount;}
+		double feedToGramAvg = 0.0;
+		if (feedToGramCount != 0) {feedToGramAvg = (double)feedToGramSum/feedToGramCount;}
+		double walkCntAvg = 0.0;
+		if (walkCntCount != 0) {walkCntAvg = (double)walkCntSum/walkCntCount;}
+		double playCntAvg = 0.0;
+		if (playCntCount != 0) {playCntAvg = (double)playCntSum/playCntCount;}
+		double urinationAvg = 0.0;
+		if (urinationCount != 0) {urinationAvg = (double)urinationSum/urinationCount;}
+		double defecationAvg = 0.0;
+		if (defecationCount != 0) {defecationAvg = (double)defecationSum/defecationCount;}
+		double respiratoryRateAvg = 0.0;
+		if (respiratoryRateCount != 0) {respiratoryRateAvg = (double)respiratoryRateSum/respiratoryRateCount;}
+		double customIntAvg = 0.0;
+		if (customIntCount != 0) {customIntAvg = (double)customIntSum/customIntCount;}
+
+		return MntReportResponse.builder().weightAvg(weightAvg).weightCount(weightCount)
+				.vomitTrue(vomitTrue).vomitCount(vomitCount)
+				.amPillTrue(amPillTrue)
+				.amPillCount(amPillCount)
+				.pmPillTrue(pmPillTrue)
+				.pmPillCount(pmPillCount)
+				.customSymptomTrue(customSymptomTrue)
+				.customSymptomCount(customSymptomCount)
+				.customSymptomName(customSymptomName)
+				.feedToGramAvg(feedToGramAvg)
+				.feedToGramCount(feedToGramCount)
+				.walkCntAvg(walkCntAvg)
+				.walkCntCount(walkCntCount)
+				.playCntAvg(playCntAvg)
+				.playCntCount(playCntCount)
+				.urinationAvg(urinationAvg)
+				.urinationCount(urinationCount)
+				.defecationAvg(defecationAvg)
+				.defecationCount(defecationCount)
+				.respiratoryRateAvg(respiratoryRateAvg)
+				.respiratoryRateCount(respiratoryRateCount)
+				.customIntAvg(customIntAvg)
+				.customIntCount(customIntCount)
+				.customIntName(customIntName)
+				.build();
+	}
+
 }
