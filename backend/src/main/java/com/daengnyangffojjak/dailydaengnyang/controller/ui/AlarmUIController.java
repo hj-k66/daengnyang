6934d3b4c6a87a89217f91bb937af547d33fc5e1
naegonggae <tkdtkd97@naver.com>
2023-02-14 04:45:46 +0900

    :sparkles: feat: 캘린더 UI 배너 추가, 일정 UI, 알림리스트 UI 틀 작성

---
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/controller/ui/AlarmUIController.java
@@ -0,0 +1,20 @@
+package com.daengnyangffojjak.dailydaengnyang.controller.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/view")
+@RequiredArgsConstructor
+public class AlarmUIController {
+
+	@GetMapping("/alarm/list")
+	public String alarmList(Model model) {
+
+		return "alarm/alarm_list";
+	}
+
+}
