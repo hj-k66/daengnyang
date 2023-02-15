3005930f22aa7c463e5006352a0fedbdba6fb455
hjx66 <hjx66@g.cnu.ac.kr>
2023-02-16 08:58:35 +0900

    :memo: docs: 주석 추가

--- backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/controller/ui/HomeController.java
+++ backend/src/main/java/com/daengnyangffojjak/dailydaengnyang/controller/ui/HomeController.java
@@ -1,8 +1,6 @@
 package com.daengnyangffojjak.dailydaengnyang.controller.ui;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
+	//home
 	@GetMapping
 	public String home() {
 		return "record/record_list";
